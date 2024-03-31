47add0628d6c1e6f4a8e26489026dfec32fb5034
Patrick Morris <telavian@gmail.com>
2024-03-31 15:23:44 -0700

    Remove extra

--- src/SimplyScriptures.Maui/.Clean.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Do you want to clean all BIN and OBJ folders
-pause
-@echo Deleting all BIN and OBJ folders
-for /d /r . %%d in (bin,obj) do @if exist "%%d" rd /s/q "%%d"
-@echo BIN and OBJ folders successfully deleted
-pause > nul
